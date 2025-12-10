67afd830f24765f2f8344b453c5aa9f82fcb239f
Hailey Lan <haileylan404@gmail.com>
2025-12-10 14:01:54 -0500

    67

--- api/src/main.js
+++ api/src/main.js
@@ -1,1 +1,7 @@
 import './style.css'
+
+fetch('http://ponyapi.net/v1/character/all')
+    .then(response => response.json())
+    .then(data => console.log(data))
+console.log()
+async function loadCards(){}
