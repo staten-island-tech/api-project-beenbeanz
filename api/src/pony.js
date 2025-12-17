15901c8eb91b7e2f7033de43cc7bc4e655cd4765
Hailey L <141077391+beenbeanz@users.noreply.github.com>
2025-12-17 13:04:54 -0500

    wed

--- api/src/pony.js
+++ api/src/pony.js
@@ -6,6 +6,11 @@ if (ponyName) {
     showPonyData(ponyName);
 }
 
+const allPoniesBtn = document.querySelector('#allPoniesBtn');
+allPoniesBtn.addEventListener('click', () => {
+    window.location.href = `./index.html`;
+})
+
 export async function showPonyData(ponyName){
     const ponyNameForUrl = ponyName.replaceAll(' ', '_');
 
@@ -25,7 +30,9 @@ export async function showPonyData(ponyName){
             image
         } = pony
 
-        const aliasHTML = alias ? alias : 'none';
+        const aliasHTML = alias ? alias : "Oops! No data found";
+        const occupationHTML = occupation ? occupation : "Oops! No data found";
+        const residenceHTMl = residence ? residence : "Oops! No data found";
 
         const ponyInfoSection = document.querySelector('#ponyInfo')
         ponyInfoSection.insertAdjacentHTML('beforeend', 
@@ -34,8 +41,8 @@ export async function showPonyData(ponyName){
             <h4 class='alias m-0.5'>Aliase(s): ${aliasHTML}</h4>
             <a class='url underline m-0.5' href='${url}'>Wiki page</a>
             <p class='sex normalText m-0.5'>Sex: ${sex}</p>
-            <p class='residence normalText m-0.5'>Place(s) of residency: ${residence}</p>
-            <p class='occupation normalText m-0.5'>Occupation(s): ${occupation}</p>
+            <p class='residence normalText m-0.5'>Place(s) of residency: ${residenceHTMl}</p>
+            <p class='occupation normalText m-0.5'>Occupation(s): ${occupationHTML}</p>
             <p class='kind normalText m-0.5'>Kind(s): ${kind}</p>
             <p class='id normalText m-0.5'>ID: ${id}</p>
             <div id='imgDiv' class="flex flex-wrap justify-center"></div>
