24a0949ac5b1edd3be7e4d134a6283d4c4a32aff
Hailey Lan <haileylan404@gmail.com>
2025-12-22 13:42:49 -0500

    fihwr

--- api/vite.config.js
+++ api/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        pony: resolve(__dirname, 'pony.html'),
+      },
+    },
+  },
 })
